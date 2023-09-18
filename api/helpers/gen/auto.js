ac2acf665fb736bf5bfdbeb1480012f4337c6479
JuanDavidAvilaRaveloCampus <juandavidavilaravelo856@gmail.com>
2023-09-18 07:22:22 -0500

    fix: :fire: validation .env autogen
    now, this script validates whether this file exists or not

--- api/helpers/gen/auto.js
+++ api/helpers/gen/auto.js
@@ -1,28 +1,39 @@
 import fs from 'fs/promises';
 
+const archivo = '.env';
 const contenido = `ATLAS_USER=""
 ATLAS_PASSWORD=""
 ATLAS_DB="incidents"
-JWT_PRIVATE_KEY="soy_la_key"
-MY_SERVER={"hostname":"localhost","port":5010}
+JWT_PRIVATE_KEY="im_the_key"
+MY_SERVER={"hostname":"localhost","port":5510}
 `;
 
-const archivo = '.env';
-
-const existencia = fs.exist(archivo);
-
-if(existencia){
-    return console.log("El archivo existe");
+// Función para verificar si un archivo existe
+async function archivoExiste(archivo) {
+    try {
+        await fs.access(archivo);
+        return true; // El archivo existe
+    } catch (error) {
+        return false; // El archivo no existe
+    }
 }
 
-else {
-    (async () => {
-        try {
-            await fs.writeFile(archivo, contenido);
-            console.log(`Se ha creado el archivo "${archivo}"`);
-            console.warn("No te olvides de agregar tus credenciales en el archivo .env\n");
-        } catch (err) {
-            console.error('Error al crear el archivo:', err);
+// Verificar si el archivo existe antes de escribir en él
+archivoExiste(archivo)
+    .then((existe) => {
+        if (existe) {
+            console.log(`El archivo ${archivo} ya existe.`);
+        } else {
+            // Escribe el contenido en el archivo si no existe
+            fs.writeFile(archivo, contenido)
+                .then(() => {
+                    console.log(`Se ha escrito el contenido en ${archivo}`);
+                })
+                .catch((error) => {
+                    console.error(`Error al escribir en ${archivo}: ${error.message}`);
+                });
         }
-    })();
-}
+    })
+    .catch((error) => {
+        console.error(`Error al verificar la existencia de ${archivo}: ${error.message}`);
+    });
