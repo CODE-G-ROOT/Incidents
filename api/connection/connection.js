1ba9b111d946e00393bbdf909feaebf0cd0ea2f5
JuanDavidAvilaRaveloCampus <juandavidavilaravelo856@gmail.com>
2023-09-18 13:09:24 -0500

    feat: :sparkles: Implements DB
    settings file the database

---
+++ api/connection/connection.js
@@ -0,0 +1,19 @@
+import { MongoClient } from "mongodb";
+
+let conn = undefined;
+
+const connect_String = '';
+const cliente = new MongoClient(connect_String);
+
+try {
+    conn = await cliente.connect();
+} catch (error) {
+    console.error(error);
+};
+
+let db = conn.db('incidets_system');
+
+export default db;
+
+
+
