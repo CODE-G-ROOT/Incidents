d99f5387c1f2ae4cdfe7fe0e4c73ea46ab7803d0
JuanDavidAvilaRaveloCampus <juandavidavilaravelo856@gmail.com>
2023-09-19 22:40:19 -0500

    feat: :construction: JWT in progress
    There was missing add the validation of login

---
+++ api/helpers/JWT.js
@@ -0,0 +1,50 @@
+import { SignJWT, jwtVerify } from "jose"
+import db from "../connection/connection.js"
+import { ObjectId } from "mongodb";
+import data from '../../auto_setting.js'
+
+const db = await connect();
+export const createToken = async (req, res, next) => {
+    if (Object.keys(req.body).length === 0)
+        return res.status(400).send({ message: "Data not founded" });
+
+    const result = await db.collection('users').findOne(req.body);
+
+    if (JSON.stringify(Object.keys(req.body)) !== JSON.stringify(['name', 'id'])) //el usuario es el discord y su id es el identificador unico de discord
+        return res.status(417).send({ message: "Para iniciar sesion se require: name [nombre del usuario] y id [password]" })
+
+    if (!result)
+        return res.status(401).send({ mesaage: "session no encontrada" });
+
+    const encoder = new TextEncoder();
+    const id = result._id.toString();
+    const jwtConstructor = await new SignJWT({ id: id })
+        .setProtectedHeader({ alg: 'HS256', typ: 'JWT' })
+        .setIssuedAt()
+        .setExpirationTime('3h')
+        .sign(encoder.encode(data.key));
+    req.data = { status: 200, message: jwtConstructor };
+    next();
+}
+
+export const verifyToken = async (req, token) => {
+    try {
+        const encoder = new TextEncoder();
+        const jwtData = await jwtVerify(
+            token,
+            encoder.encode(CONFIG.key)
+        );
+        let res = await db.collection('session').findOne(
+            {
+                _id: new ObjectId(jwtData.payload.id),
+                [`permisos.${req.baseUrl}`]: `${req.headers["accept-version"]}`
+            }
+        );
+        const error = { message: "no tienes acceso a este método" }
+        if (!res.permisos[req.baseUrl].includes(req.method)) return error;
+        let { _id, permisos, ...session } = res;
+        return session;
+    } catch (error) {
+        return false;
+    }
+}
