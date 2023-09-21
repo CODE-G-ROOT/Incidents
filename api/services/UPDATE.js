a446746bc385215559fa718f8effd3dbf9652e19
JuanDavidAvilaRaveloCampus <juandavidavilaravelo856@gmail.com>
2023-09-21 16:08:39 -0500

    feat: :fire: Remove files
    this files has been remove, for system clean

--- api/services/UPDATE.js
+++
@@ -1,7 +0,0 @@
-import Incidents from "../collections/incidents.js";
-
-const incident = new Incidents();
-
-export const update_incidence = async(id) => {
-    return await incident.update_incidence({"id": parseInt(id)});
-};
