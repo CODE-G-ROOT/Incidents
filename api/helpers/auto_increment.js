1ac87e34d17d539a7cabf78d71ff263334811671
JuanDavidAvilaRaveloCampus <juandavidavilaravelo856@gmail.com>
2023-09-23 05:06:55 -0500

    feat: :wrench: All changed in this code
    Has been changed the connection and has been error handle

--- api/helpers/auto_increment.js
+++ api/helpers/auto_increment.js
@@ -1,13 +1,21 @@
-import db from '../../connection/conect.js'
+import { connect } from '../connection/connection.js'; // Importa la función 'connect' correctamente
 
-export default async function autoIncrementar (categoria, id){
+export default async function autoIncrementar(categoria, id) {
     use("incidents_system");
-    let data = db[categoria]
-        .find()
-        .sort({[id]: -1})
-        .toArray();
-    if(data.length < 1) return 1; 
     
-    let lastNumber = data[0][id];
-    return lastNumber + 1;
+    try {
+        const db = await connect(); // Utiliza la función 'connect' para obtener la conexión a la base de datos
+        const data = await db.collection(categoria) // Utiliza la categoría para seleccionar la colección
+            .find()
+            .sort({ [id]: -1 })
+            .toArray();
+
+        if (data.length < 1) return 1;
+
+        const lastNumber = data[0][id];
+        return lastNumber + 1;
+    } catch (error) {
+        console.error(error);
+        throw error; // Maneja errores apropiadamente
+    }
 };
