e59fd824dd38f9bd3191561acd246a1cf82e286e
JuanDavidAvilaRaveloCampus <juandavidavilaravelo856@gmail.com>
2023-09-25 01:19:54 -0500

    fix: :bug: Response and error manage
    The error and response manage has been fixed
    
    #  3c87b5dfe29bd74115c305d4776a9e2c15a3d16d

--- api/controllers/v2/POST_incidenes.js
+++ api/controllers/v2/POST_incidenes.js
@@ -2,20 +2,19 @@ import { rateLimit } from "express-rate-limit";
 import { post_incidence } from "../../services/POST.js";
 
 export const post_incidence_controller = async (req, res, next) => {
-    if (!rateLimit) return;
     try {
 
         const data = req.body
         const incidence = await post_incidence(data);
 
+        if(incidence.error === 302)  return res.status(302).send(incidence)
+
         incidence.length > 0
-            ? res.status(302).send(user)
+            ? res.status(200).send({status:200, message: "The document has been sent"})
             : res.status(400).send({
                 message: "Query not found",
                 reference: "https://http.cat/400"
             });
-
-        console.log(req.rateLimit);
 
     } catch (error) {
         console.error(error);
