cccb513fae55c76b476c0e1cd513b0c4ec3074d4
JuanDavidAvilaRaveloCampus <juandavidavilaravelo856@gmail.com>
2023-09-26 02:02:01 -0500

    feat: :boom: Error management
    new error managment has been implemented

--- api/controllers/v2/POST_incidenes.js
+++ api/controllers/v2/POST_incidenes.js
@@ -1,20 +1,16 @@
-import { rateLimit } from "express-rate-limit";
+// import { rateLimit } from "express-rate-limit";
 import { post_incidence } from "../../services/POST.js";
 
 export const post_incidence_controller = async (req, res, next) => {
     try {
 
-        const data = req.body
+        const data = req.body;
         const incidence = await post_incidence(data);
         console.log(incidence);
-        if(incidence.error === 302)  return res.status(302).send(incidence)
 
-        incidence.length > 0
-            ? res.status(200).send({status:200, message: "The document has been sent"})
-            : res.status(400).send({
-                message: "Query not found",
-                reference: "https://http.cat/400"
-            });
+        incidence.error === 302
+            ? res.status(302).send(incidence)
+            : res.status(200).send({ status: 200, message: "The document has been successfully sent" })
 
     } catch (error) {
         console.error(error);
