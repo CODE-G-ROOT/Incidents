f3b048e873989dbafa2e32fbc2acca72ea616a69
CampoSuarezJavierDavidCampus <campo.javier1502@gmail.com>
2023-09-30 11:46:06 -0500

    feat: :construction: Contrucción login
    it's in progress

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './components/Bienvenidos.jsx'
-import './index.css'
+// import App from './components/Bienvenidos.jsx'
+
+import Login from './components/Log_In'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Login />
   </React.StrictMode>,
 )
