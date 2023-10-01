b28b0cbe180e262a1e9e2c1bb03bcd9a6ffbde9c
JuanDavidAvilaRaveloCampus <juandavidavilaravelo856@gmail.com>
2023-10-01 00:34:22 -0500

    feat: :sparkles: New Structure files
    has been implemented new structures files by the use front-end

---
+++ src/components/menu/Navbar_incidences_EQUIPMENT.jsx
@@ -0,0 +1,25 @@
+import { NextUIProvider } from "@nextui-org/react";
+
+function Navbar_incidences_EQUIPMENT() {
+    // const [count, setCount] = useState(0)
+
+    return (
+        <>
+            <NextUIProvider>
+                <div>
+                    <div className="container_list_menu">
+                        <div className="container_list">
+                            <ul>
+                                <li>Name</li>
+                                <li>Marc</li>
+                                <li>Code</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </NextUIProvider>
+        </>
+    )
+}
+
+export default Navbar_incidences_EQUIPMENT
