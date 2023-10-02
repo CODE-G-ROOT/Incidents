3cca579d7a9f5a5476445f045d0ca63ea2cd4f2f
JuanDavidAvilaRaveloCampus <juandavidavilaravelo856@gmail.com>
2023-10-02 02:07:04 -0500

    feat: :sparkles: new vite settgins
    variables enviroment has been implemented in settgins vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import data from './auto_setting.js'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: data.SERVER_FRONT.port,
+    host: data.SERVER_FRONT.host
+  }
 })
