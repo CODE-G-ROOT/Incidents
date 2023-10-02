748aa5c86f6599b4a27f4c349867ba9b21b57b08
JuanDavidAvilaRaveloCampus <juandavidavilaravelo856@gmail.com>
2023-10-02 09:32:01 -0500

    feat: :construction: Sidebar component in costruction

---
+++ src/components/ListboxWrapper.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const ListboxWrapper = ({children}) => (
+  <div className="w-[260px] px-1 py-2">
+    {children}
+  </div>
+);
--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,16 @@ import ReactDOM from 'react-dom/client'
 // import App from './components/Bienvenidos.jsx'
 
 //? NEXT UI
+import {NextUIProvider} from "@nextui-org/system";
 
-import { Oficial_Login } from './components/Oficial_Login';
-
-// import { Discord_svg } from './assets/SVG';
+// import { Oficial_Login } from './components/Oficial_Login';
+import { Menu_Incidences } from './components/List';
+// import { Menu_Complete } from './components/List';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Oficial_Login>
-
-    </Oficial_Login>
+    <NextUIProvider>
+      <Menu_Incidences />
+    </NextUIProvider>
   </React.StrictMode>,
 )
