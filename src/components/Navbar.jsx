1b29eb408f047077d62771202ce2d8b8dd2889fa
JuanDavidAvilaRaveloCampus <juandavidavilaravelo856@gmail.com>
2023-10-03 15:59:13 -0500

    feat: :sparkles: NavBar for Rol
    Has been implemented Navbar for rol

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,364 @@
+import React from "react";
+
+//? SVG FILES
+import Title_line from '../assets/svg/Title_line.svg'
+
+import { Avatar } from "@nextui-org/react";
+
+export function Navbar_Admin() {
+    return (
+        <>
+            <button
+                data-drawer-target="default-sidebar"
+                data-drawer-toggle="default-sidebar"
+                aria-controls="default-sidebar"
+                type="button"
+                className="inline-flex items-center p-2 mt-2 ml-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            >
+                <span className="sr-only">Open sidebar</span>
+                <svg
+                    className="w-6 h-6"
+                    aria-hidden="true"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <path
+                        clipRule="evenodd"
+                        fillRule="evenodd"
+                        d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"
+                    ></path>
+                </svg>
+            </button>
+
+            <aside
+                id="default-sidebar"
+                className="fixed top-0 left-0 z-40 w-96 h-screen transition-transform -translate-x-full sm:translate-x-0 "
+                aria-label="Sidebar"
+            >
+                <div className="h-full px-6 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
+                <ul className="font-medium">
+                    <Exit_User></Exit_User>
+                    <Menu_Incidences></Menu_Incidences>
+                    <Menu_Users></Menu_Users>
+                    <User_Logo_Discord></User_Logo_Discord>
+                </ul>
+                </div>
+            </aside>
+        </>
+    )
+};
+
+export function Navbar_User() {
+    return (
+        <>
+            <button
+                data-drawer-target="default-sidebar"
+                data-drawer-toggle="default-sidebar"
+                aria-controls="default-sidebar"
+                type="button"
+                className="inline-flex items-center p-2 mt-2 ml-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            >
+                <span className="sr-only">Open sidebar</span>
+                <svg
+                    className="w-6 h-6"
+                    aria-hidden="true"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <path
+                        clipRule="evenodd"
+                        fillRule="evenodd"
+                        d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"
+                    ></path>
+                </svg>
+            </button>
+
+            <aside
+                id="default-sidebar"
+                className="fixed top-0 left-0 z-40 w-96 h-screen transition-transform -translate-x-full sm:translate-x-0 "
+                aria-label="Sidebar"
+            >
+                <div className="h-full px-6 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
+                <ul className="font-medium">
+                    <Exit_User></Exit_User>
+                    <Menu_Incidences></Menu_Incidences>
+                    <User_Logo_Discord></User_Logo_Discord>
+                </ul>
+                </div>
+            </aside>
+        </>
+    )
+};
+
+export function Menu_Incidences_Admin() {
+    return (
+        <>
+            <button
+                data-drawer-target="default-sidebar"
+                data-drawer-toggle="default-sidebar"
+                aria-controls="default-sidebar"
+                type="button"
+                className="inline-flex items-center p-2 mt-2 ml-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            >
+                <span className="sr-only">Open sidebar</span>
+                <svg
+                    className="w-6 h-6"
+                    aria-hidden="true"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <path
+                        clipRule="evenodd"
+                        fillRule="evenodd"
+                        d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"
+                    ></path>
+                </svg>
+            </button>
+
+            <aside
+                id="default-sidebar"
+                className="fixed top-0 left-0 z-40 w-64 h-auto transition-transform -translate-x-full sm:translate-x-0"
+                aria-label="Sidebar"
+            >
+                <Menu_Incidences></Menu_Incidences>
+                <Menu_Users></Menu_Users>
+                <div className="flex">
+                    <User_Logo_Discord></User_Logo_Discord>
+                    <Exit_User></Exit_User>
+                </div>
+            </aside>
+        </>
+    );
+};
+
+export function Menu_Incidences() {
+    return (
+        <>
+            <div className="h-full px-6 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
+                <ul className="font-medium">
+
+                    <li className="my-10">
+                        <div className="text-center">
+                            <span className="w-full h-8 text-center text-white text-3xl font-normal cursor-default font-['Noto Sans'] leading-tight">INCIDENTS</span>
+                            <div className="w-full">
+                                <img src={Title_line} />
+                            </div>
+                        </div>
+                    </li>
+
+                    <div className="space-y-2  mt-5 pl-3">
+                        <li>
+                            <a
+                                href="#"
+                                className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white group"
+                            >
+                                <svg
+                                    className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                                    aria-hidden="true"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="currentColor"
+                                    viewBox="0 0 18 18"
+                                >
+                                    <path d="M16 14V2a2 2 0 0 0-2-2H2a2 2 0 0 0-2 2v15a3 3 0 0 0 3 3h12a1 1 0 0 0 0-2h-1v-2a2 2 0 0 0 2-2ZM4 2h2v12H4V2Zm8 16H3a1 1 0 0 1 0-2h9v2Z" />
+                                </svg>
+                                <span className="flex-1 ml-3 whitespace-nowrap">ALL</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                href="#"
+                                className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white group"
+                            >
+                                <svg
+                                    className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                                    aria-hidden="true"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="currentColor"
+                                    viewBox="0 0 18 20"
+                                >
+                                    <path d="M17 5.923A1 1 0 0 0 16 5h-3V4a4 4 0 1 0-8 0v1H2a1 1 0 0 0-1 .923L.086 17.846A2 2 0 0 0 2.08 20h13.84a2 2 0 0 0 1.994-2.153L17 5.923ZM7 9a1 1 0 0 1-2 0V7h2v2Zm0-5a2 2 0 1 1 4 0v1H7V4Zm6 5a1 1 0 1 1-2 0V7h2v2Z" />
+                                </svg>
+
+                                <span className="flex-1 ml-3 whitespace-nowrap">EQUIPMENT</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                href="#"
+                                className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white group"
+                            >
+                                <svg
+                                    className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                                    aria-hidden="true"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="currentColor"
+                                    viewBox="0 0 18 20"
+                                >
+                                    <path d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z" />
+                                </svg>
+                                <span className="flex-1 ml-3 whitespace-nowrap">LOCATION</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                href="#"
+                                className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white group"
+                            >
+                                <svg
+                                    className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                                    aria-hidden="true"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="currentColor"
+                                    viewBox="0 0 20 20"
+                                >
+                                    <path d="m17.418 3.623-.018-.008a6.713 6.713 0 0 0-2.4-.569V2h1a1 1 0 1 0 0-2h-2a1 1 0 0 0-1 1v2H9.89A6.977 6.977 0 0 1 12 8v5h-2V8A5 5 0 1 0 0 8v6a1 1 0 0 0 1 1h8v4a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-4h6a1 1 0 0 0 1-1V8a5 5 0 0 0-2.582-4.377ZM6 12H4a1 1 0 0 1 0-2h2a1 1 0 0 1 0 2Zm0-5a2 2 0 1 1 4 0v1H7V4Zm6 5a1 1 0 1 1-2 0V7h2v2Z" />
+                                </svg>
+                                <span className="flex-1 ml-3 whitespace-nowrap">BY REPORTS</span>
+                            </a>
+                        </li>
+                    </div>
+                </ul>
+            </div>
+        </>
+    );
+};
+
+export function Menu_Users() {
+    return (
+        <>
+            <div className="h-full px-6 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
+                <li className="my-10">
+                    <div className="text-center">
+                        <span className="w-full h-8 text-center text-white text-3xl font-normal font-['Noto Sans'] leading-tight cursor-default">USERS</span>
+                        <div className="w-full">
+                            <img src={Title_line} />
+                        </div>
+                    </div>
+                </li>
+
+                <div className="space-y-2 mt-5 pl-3">
+                    <li>
+                        <a
+                            href="#"
+                            className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white group"
+                        >
+                            <svg
+                                className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                                aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="currentColor"
+                                viewBox="0 0 18 18"
+                            >
+                                <path d="M16 14V2a2 2 0 0 0-2-2H2a2 2 0 0 0-2 2v15a3 3 0 0 0 3 3h12a1 1 0 0 0 0-2h-1v-2a2 2 0 0 0 2-2ZM4 2h2v12H4V2Zm8 16H3a1 1 0 0 1 0-2h9v2Z" />
+                            </svg>
+                            <span className="flex-1 ml-3 whitespace-nowrap">ALL</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href="#"
+                            className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white group"
+                        >
+                            <svg
+                                className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                                aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="currentColor"
+                                viewBox="0 0 20 20"
+                            >
+                                <path d="M18 0H6a2 2 0 0 0-2 2h14v12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Z" />
+                                <path d="M14 4H2a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2ZM2 16v-6h12v6H2Z" />
+                            </svg>
+                            <span className="flex-1 ml-3 whitespace-nowrap">ID</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href="#"
+                            className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white group"
+                        >
+                            <svg
+                                className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                                aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="currentColor"
+                                viewBox="0 0 20 20"
+                            >
+                                <path d="M7.958 19.393a7.7 7.7 0 0 1-6.715-3.439c-2.868-4.832 0-9.376.944-10.654l.091-.122a3.286 3.286 0 0 0 .765-3.288A1 1 0 0 1 4.6.8c.133.1.313.212.525.347A10.451 10.451 0 0 1 10.6 9.3c.5-1.06.772-2.213.8-3.385a1 1 0 0 1 1.592-.758c1.636 1.205 4.638 6.081 2.019 10.441a8.177 8.177 0 0 1-7.053 3.795Z" />
+                            </svg>
+                            <span className="flex-1 ml-3 whitespace-nowrap">ROL</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href="#"
+                            className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white group"
+                        >
+                            <svg
+                                className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                                aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="currentColor"
+                                viewBox="0 0 20 20"
+                            >
+                                <path d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z" />
+                            </svg>
+                            <span className="flex-1 ml-3 whitespace-nowrap">NAME</span>
+                        </a>
+                    </li>
+                </div>
+            </div>
+            <div className="h-full px-6 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
+                <ul className="font-medium"></ul>
+
+            </div>
+        </>);
+};
+
+export function User_Logo_Discord() {
+    return (
+        <div className="w-full absolute bottom-5 flex px-0">
+            <Avatar
+                className="w-20 h-20 text-large text-white "
+                size="lg"
+                src="https://i.pravatar.cc/150?u=a04258114e29026708c"
+            />
+            <d10v className="flex flex-col">
+                <h2 className="text-white w-10 h-10 p-3 text-large"><b>JuanAvila8856</b></h2>
+                <span className="text-white w-1 h-1 p-3 pt-0 ">(user)</span>
+            </d10v>
+        </div>
+    )
+};
+
+export function Exit_User() {
+    return (
+        <>
+            <a
+                href="/home"
+                className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+            >
+                <svg
+                    className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 18 16"
+                >
+                    <path
+                        stroke="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M1 8h11m0 0L8 4m4 4-4 4m4-11h3a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-3"
+                    />
+                </svg>
+                <span className="flex-1 ml-3 whitespace-nowrap">Exit</span>
+            </a>
+        </>
+    )
+};
+
--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+//? React
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
@@ -6,60 +7,16 @@ import {NextUIProvider} from "@nextui-org/system";
 
 
 //? Router Dep
-import { createBrowserRouter, RouterProvider, } from "react-router-dom";
+import { createBrowserRouter, RouterProvider} from "react-router-dom";
 
 
 // ? Routers Directions
-import { Menu_Incidences } from './components/List';
-import { Menu_Complete } from './components/List';
-// import { Search_Button } from './components/Background_panel';
-// import Error_404 from './components/Error_404';
-import { Oficial_Login } from './components/Oficial_Login';
-// import { Error_401 } from './components/Errors_';
-
-//? Routers Management
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <Root />,
-//     errorElement: <ErrorPage />,
-//     loader: rootLoader,
-//     action: rootAction,
-//     children: [
-//       {
-//         errorElement: <ErrorPage />,
-//         children: [
-//           {
-//             index: true, element: <Index />
-//           },
-//           {
-//             path: "contacts/:contactId",
-//             element: <Contact />,
-//             loader: contactLoader,
-//             action: contactAction
-//           },
-//           {
-//             path: "contacts/:contactId/edit",
-//             element: <EditContact />,
-//             loader: contactLoader,
-//             action: editAction,
-//           },
-//           {
-//             path: "contacts/:contactId/destroy",
-//             action: destroyAction,
-//             errorElement: <div> Oops! There was an error. </div>
-//           }
-//         ]
-//       }
-//     ],
-//   }
-// ]);
-
+import { Navbar_Admin, Navbar_User } from './components/Navbar';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <NextUIProvider>
-      <Menu_Incidences/>
+      <Navbar_User/>
     </NextUIProvider>
   </React.StrictMode>,
 )
