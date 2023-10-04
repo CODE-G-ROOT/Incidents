e61427deed52878ab403a78d6960879f2374b930
JuanDavidAvilaRaveloCampus <juandavidavilaravelo856@gmail.com>
2023-10-04 08:23:47 -0500

    feat: :sparkles: Button aggregate incidence
    this componet has been implemented

--- src/components/Agregates.jsx
+++ src/components/Agregates.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link, ScrollShadow, Textarea, Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure } from "@nextui-org/react";
+import Frame from '../assets/svg/Frame.svg'
 
 export function Buttons_All() {
 
@@ -41,10 +42,22 @@ export function Buttons_Location() {
 };
 
 export const Button_Agregate = () => {
+
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    const [backdrop, setBackdrop] = React.useState('opaque')
+
+    const Modal_Info = (backdrop) => {
+        setBackdrop(backdrop)
+        onOpen();
+    }
+
     return (
+
+
+
         <>
             <div className="absolute bottom-5 right-5">
-                <button title="Add New" class="group cursor-pointer hover:rotate-90 active:scale-100 duration-200">
+                <button title="Add New" class="group cursor-pointer hover:rotate-90 active:scale-100 duration-200" onClick={() => Modal_Info('blur')}>
                     <svg xmlns="http://www.w3.org/2000/svg" width="65px" height="65px" viewBox="0 0 24 24" class="stroke-slate-600 fill-none group-active:fill-slate-200 duration-200">
                         <path d="M12 22C17.5 22 22 17.5 22 12C22 6.5 17.5 2 12 2C6.5 2 2 6.5 2 12C2 17.5 6.5 22 12 22Z" stroke-width="1.5"></path>
                         <path d="M8 12H16" stroke-width="1.5"></path>
@@ -52,6 +65,28 @@ export const Button_Agregate = () => {
                     </svg>
                 </button>
             </div>
+            <Modal backdrop={backdrop} isOpen={isOpen} onClose={onClose} size="5xl" >
+                <ModalContent className="bg-white ">
+                    {(onClose) => (
+                        <>
+                            <ModalHeader className="flex flex-col gap-5 text-white">Incidence</ModalHeader>
+                            <ModalBody className="mt-96">
+                                <Modal_New_Incidence></Modal_New_Incidence>
+                            </ModalBody>
+                            <ModalFooter>
+
+                                <Button color="danger" variant="light" onPress={onClose}>
+                                    Close
+                                </Button>
+                                <Button color="success" variant="light" onPress={onClose}>
+                                    Send
+                                </Button>
+
+                            </ModalFooter>
+                        </>
+                    )}
+                </ModalContent>
+            </Modal>
 
         </>
     )
@@ -504,6 +539,139 @@ export function Modal_Info_Incidence() {
     )
 };
 
+export function Modal_New_Incidence() {
+
+    const equip = {
+        name: "mouse",
+        marc: "legion",
+        code: "0007"
+    };
+
+    const locate = {
+        area: "Campus",
+        room: "Review"
+    };
+
+    return (
+        <div className="full flex flex-col p-20 space-y-10">
+            {/* simulando un header */}
+            <div className="flex w-full justify-between">
+                <div className="w-full flex gap-10 justify-between">
+                    <div className="flex flex-col w-1/2 gap-2">
+                        <h3 className="text-2xl font-semibold leading-7 text-gray-900">ID:</h3>
+                        <p className=" max-w-2xl text-xl leading-7 text-gray-500">546518</p>
+                    </div>
+                    <div className="w-1/2 flex flex-col gap-5 ">
+                        <div className="flex space-x-20 ">
+                            <div className="px-4 sm:px-0">
+                                <h3 className="text-base font-semibold leading-7 text-gray-900">Creation</h3>
+                                <p className="mt-1 max-w-2xl text-sm leading-6 text-gray-500">Personal details and application.</p>
+                            </div>
+                            <div className="px-4 sm:px-0">
+                                <h3 className="text-base font-semibold leading-7 text-gray-900">Last Update:</h3>
+                                <p className="mt-1 max-w-2xl text-sm leading-6 text-gray-500">Personal details and application.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {/* Fechas y ID */}
+
+            </div>
+
+
+            {/* el body flex col*/}
+            <div>
+                <dl className="divide-y divide-gray-100">
+                    <div className="px-4 py-6 sm:grid sm:grid-cols-2 sm:gap-4 sm:px-0">
+                        <dt className="text-base font-semibold leading-7 text-gray-900">Report By:</dt>
+                        <Textarea
+                            labelPlacement="outside"
+                            placeholder="Text"
+                            defaultValue=""
+                            className="max-w-ls"
+                        />
+                    </div>
+                    <div className="px-4 py-6 sm:grid sm:grid-cols-2 sm:gap-4 sm:px-0">
+                        <dt className="text-sm font-medium leading-6 text-gray-900">Category</dt>
+                        <Textarea
+                            labelPlacement="outside"
+                            placeholder="Text"
+                            defaultValue=""
+                            className="max-w-ls"
+                        />
+                    </div>
+                    <div className="px-4 py-6 sm:grid sm:grid-cols-2 sm:gap-4 sm:px-0">
+                        <dt className="text-sm font-medium leading-6 text-gray-900">Type</dt>
+                        <Textarea
+                            labelPlacement="outside"
+                            placeholder="Text"
+                            defaultValue=""
+                            className="max-w-ls"
+                        />
+                    </div>
+                    <div className="px-4 py-6 sm:grid sm:grid-cols-2 sm:gap-4 sm:px-0">
+                        <dt className="text-sm font-medium leading-6 text-gray-900">Equipment</dt>
+                        <dd className="flex gap-2">
+                            {Object.keys(equip).map((data) => (
+                                <Textarea
+                                    labelPlacement="outside"
+                                    placeholder={data}
+                                    defaultValue=""
+                                    className="max-w-ls"
+                                />
+                            ))}
+                        </dd>
+                    </div>
+                    <div className="px-4 py-6 sm:grid sm:grid-cols-2 sm:gap-4 sm:px-0">
+                        <dt className="text-sm font-medium leading-6 text-gray-900">Location</dt>
+                        <dd className="flex gap-2">
+                            {Object.keys(locate).map((data) => (
+                                <Textarea
+                                    labelPlacement="outside"
+                                    placeholder={data}
+                                    defaultValue=""
+                                    className="max-w-ls"
+                                />
+                            ))}
+                        </dd>
+                    </div>
+                    <div className="px-4 py-6 sm:grid sm:grid-cols-2 sm:gap-4 sm:px-0">
+                        <dt className="text-sm font-medium leading-6 text-gray-900">Description</dt>
+                        <Textarea
+                            labelPlacement="outside"
+                            placeholder="Text"
+                            defaultValue=""
+                            className="max-w-ls"
+                        />
+                    </div>
+                    <div className="px-4 py-6 flex flex-col sm:gap-4 sm:px-0">
+                        <dt className="text-sm font-medium leading-6 text-gray-900">DESCRIPTION</dt>
+                        <Textarea
+                            labelPlacement="outside"
+                            placeholder="Enter your description"
+                            defaultValue=""
+                            className="max-w-ls"
+                        />
+                    </div>
+                    <div className="px-4 py-6 flex flex-col sm:gap-4 sm:px-0">
+                        <dt className="text-sm w-full font-medium leading-6 text-gray-900">OBSERVACIÓN</dt>
+                        <dd className="mt-1 w-full text-sm leading-6 text-gray-500 justify-end">
+                            <Textarea
+                                isDisabled
+                                labelPlacement="outside"
+                                placeholder="Enter your description"
+                                defaultValue=""
+                                className="max-w-ls"
+                            />
+                        </dd>
+                    </div>
+                </dl>
+            </div>
+
+        </div>
+    )
+};
+
 
 export const Prueba = () => {
     const miObjeto = {
@@ -522,4 +690,36 @@ export const Prueba = () => {
         </div>
     );
 
-}
+};
+
+
+
+
+export const HeartIcon = ({
+    fill = 'currentColor',
+    filled,
+    size,
+    height,
+    width,
+    label,
+    ...props
+}) => {
+    return (
+        <svg
+            width={size || width || 24}
+            height={size || height || 24}
+            viewBox="0 0 575 575"
+            fill={filled ? fill : 'none'}
+            xmlns="http://www.w3.org/2000/svg"
+            {...props}
+        >
+            <path
+                d="M487.073 87.9262C482.54 83.4261 476.855 80.2624 470.642 78.783C464.43 77.3038 457.928 77.5659 451.854 79.5408L101.344 196.457C94.8432 198.517 89.099 202.457 84.8363 207.78C80.5736 213.103 77.984 219.569 77.3944 226.363C76.8048 233.156 78.2415 239.972 81.5233 245.949C84.8054 251.927 89.7849 256.797 95.8334 259.948L241.26 331.823L313.136 477.729C316.025 483.439 320.445 488.235 325.903 491.577C331.361 494.919 337.642 496.678 344.042 496.656H346.438C353.292 496.151 359.835 493.597 365.219 489.323C370.602 485.051 374.577 479.258 376.625 472.698L495.219 123.145C497.337 117.106 497.696 110.589 496.254 104.354C494.812 98.1193 491.628 92.4222 487.073 87.9262ZM116.198 229.52L422.146 127.457L252.281 297.323L116.198 229.52ZM345.719 458.802L277.677 322.719L447.542 152.853L345.719 458.802Z"
+                stroke={fill}
+                strokeWidth={1.5}
+                strokeLinecap="round"
+                strokeLinejoin="round"
+            />
+        </svg>
+    );
+};
