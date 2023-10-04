e97bb4bfec9b5f1db61ea0c8ebdc6a96f7f39726
JuanDavidAvilaRaveloCampus <juandavidavilaravelo856@gmail.com>
2023-10-04 08:58:43 -0500

    feat: :bug: Window modals
    now you can see better

--- src/components/Agregates.jsx
+++ src/components/Agregates.jsx
@@ -439,7 +439,7 @@ export function Modal_Info_Incidence() {
 
 
     return (
-        <div className="full flex flex-col mt-20 space-y-10">
+        <div className="full flex flex-col p-14 mt-20 space-y-10">
             {/* simulando un header */}
             <div className="flex w-full justify-between">
                 <div className="w-full flex gap-10 justify-between">
@@ -552,7 +552,7 @@ export function Modal_New_Incidence() {
     };
 
     return (
-        <div className="full flex flex-col p-20 space-y-10">
+        <div className="full flex flex-col p-14 mt-20 space-y-10">
             {/* simulando un header */}
             <div className="flex w-full justify-between">
                 <div className="w-full flex gap-10 justify-between">
@@ -583,12 +583,7 @@ export function Modal_New_Incidence() {
                 <dl className="divide-y divide-gray-100">
                     <div className="px-4 py-6 sm:grid sm:grid-cols-2 sm:gap-4 sm:px-0">
                         <dt className="text-base font-semibold leading-7 text-gray-900">Report By:</dt>
-                        <Textarea
-                            labelPlacement="outside"
-                            placeholder="Text"
-                            defaultValue=""
-                            className="max-w-ls"
-                        />
+                        <dd dd className="mt-1 max-w-2xl text-sm leading-6 text-gray-500">ASDFKJASDLFÑKJ</dd>
                     </div>
                     <div className="px-4 py-6 sm:grid sm:grid-cols-2 sm:gap-4 sm:px-0">
                         <dt className="text-sm font-medium leading-6 text-gray-900">Category</dt>
@@ -670,4 +665,20 @@ export function Modal_New_Incidence() {
         </div>
     )
 };
+
+
+export const DeleteButton = () => {
+    return (
+        <>
+            <button class="Btn">
+
+                <div class="sign text-white"><svg viewBox="0 0 512 512"><path d="M377.9 105.9L500.7 228.7c7.2 7.2 11.3 17.1 11.3 27.3s-4.1 20.1-11.3 27.3L377.9 406.1c-6.4 6.4-15 9.9-24 9.9c-18.7 0-33.9-15.2-33.9-33.9l0-62.1-128 0c-17.7 0-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32l128 0 0-62.1c0-18.7 15.2-33.9 33.9-33.9c9 0 17.6 3.6 24 9.9zM160 96L96 96c-17.7 0-32 14.3-32 32l0 256c0 17.7 14.3 32 32 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-64 0c-53 0-96-43-96-96L0 128C0 75 43 32 96 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32z"></path></svg></div>
+
+                <div class="text">Logout</div>
+            </button>
+
+        </>
+    );
+};
+
 
