05b9a62e8fd49021271c2623013e27db5993dd44
JuanDavidAvilaRaveloCampus <juandavidavilaravelo856@gmail.com>
2023-10-04 09:18:29 -0500

    feat: :sparkles: Error Page 404
    this page has been implemented

--- src/components/Errors_.jsx
+++ src/components/Errors_.jsx
@@ -85,3 +85,18 @@ export function Error_404() {
     )
 }
 
+export function Error_Page_404() {
+    return (
+        <>
+            <div class="bg-gray-800">
+                <div class="h-screen flex flex-col justify-center items-center">
+                    <h1 class="text-8xl font-bold text-white">404</h1>
+                    <p class="text-4xl font-medium text-white">Page Not Found</p>
+                    <a href="/" class="mt-4 text-xl text-blue-600 hover:underline">Go back home</a>
+                </div>
+            </div>
+        </>
+    )
+}
+
+
